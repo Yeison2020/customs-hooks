f7ae2d2874092e587f7fa7e334e1884200fd1924
Yeison <enmanuelquiterios@gmail.com>
2021-10-19 00:52:22 -0400

    Added funtionality using the useRef

--- src/components/04-useRef/RealExampleRef.js
+++ src/components/04-useRef/RealExampleRef.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import "../02-useEffect/effects.css";
+import MultipleCustomsHooks from "../03-examples/MultipleCustomsHooks";
 
 const RealExampleRef = () => {
+  // This Example hide and show my MultipleCustomsHooks Still not use of the useRef
+  const [show, setShow] = useState(false);
   return (
     <div>
       <h1>Real Ref</h1>
+      <hr />
+      {show && <MultipleCustomsHooks />}
+      <button
+        className="btn btn-primary mt-5"
+        onClick={() => {
+          setShow(!show);
+        }}
+      >
+        Show/Hide
+      </button>
     </div>
   );
 };
