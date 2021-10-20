95d86984d89190d36a795a7a9904e4e9c432039b
Yeison <enmanuelquiterios@gmail.com>
2021-10-19 20:15:35 -0400

    Adding My todo App

--- src/components/08-useReducer/TodoApp.js
+++ src/components/08-useReducer/TodoApp.js
@@ -1,11 +1,57 @@
-import React from "react";
+import React, { useReducer } from "react";
 import "./styles.css";
+import todoReducer from "./todoReducer";
+const initialState = [
+  {
+    id: new Date().getTime(),
+    desc: "Aprender React",
+    done: false,
+  },
+];
 
 const TodoApp = () => {
+  const [todos] = useReducer(todoReducer, initialState);
+  console.log(todos);
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Todo App ({todos.length})</h1>
       <hr />
+      <div className="row">
+        <div className="col-7">
+          <ul className="list-group list-group-flush">
+            {todos.map((todo, i) => {
+              return (
+                <li key={todo.id} className="list-group-item">
+                  <p className="text-center complete">
+                    {i + 1}. {todo.desc}
+                  </p>
+                  <button className="btn btn-danger">Borrar</button>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+
+        <div className="col-5">
+          <h4>Agregar todo</h4>
+          <hr />
+          <form>
+            <input
+              type="text"
+              name="description"
+              placeholder="Aprender ...."
+              autoComplete="off"
+              className="form-control"
+            ></input>
+            <button
+              type="button"
+              className="btn btn-outline-primary mt-3 btn-block"
+            >
+              Agregar
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
---
+++ src/components/08-useReducer/todoReducer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const todoReducer = (state = [], action) => {
+  switch (action.type) {
+    case "add":
+      break;
+
+    default:
+      return state;
+  }
+};
+
+export default todoReducer;
