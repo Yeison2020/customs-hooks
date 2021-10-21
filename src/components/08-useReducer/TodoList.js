00be68a1271d98fac61d995456158420fb1074c2
Yeison <enmanuelquiterios@gmail.com>
2021-10-21 10:23:32 -0400

    useContext-react

--- src/components/08-useReducer/TodoList.js
+++ src/components/08-useReducer/TodoList.js
@@ -1,30 +1,7 @@
 import React from "react";
 
 const TodoList = ({ todos, handleDelete, handleToggle }) => {
-  return (
-    <>
-      <ul className="list-group list-group-flush">
-        {todos.map((todo, i) => {
-          return (
-            <li key={todo.id} className="list-group-item">
-              <p
-                className={`${todo.done && "complete"}`}
-                onClick={() => handleToggle(todo.id)}
-              >
-                {i + 1}. {todo.desc}
-              </p>
-              <button
-                className="btn btn-danger"
-                onClick={() => handleDelete(todo.id)}
-              >
-                Borrar
-              </button>
-            </li>
-          );
-        })}
-      </ul>
-    </>
-  );
+  return <></>;
 };
 
 export default TodoList;
