4f2ed445b09a5e16f21f77bfbb4d159e5a7d9441
Yeison <enmanuelquiterios@gmail.com>
2021-11-12 00:25:15 -0500

    Completed Multiple Customs Hook

--- src/tests/components/3-examples/MultiplesCustomesHooks.test.js
+++ src/tests/components/3-examples/MultiplesCustomesHooks.test.js
@@ -1,1 +1,31 @@
-describe("", () => {});
+import React from "react";
+import { shallow } from "enzyme";
+import Enzyme from "enzyme";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import MultipleCustomsHooks from "../../../components/03-examples/MultipleCustomsHooks";
+import useFetch from "../../../hooks/useFetch";
+jest.mock("../../../hooks/useFetch");
+Enzyme.configure({ adapter: new Adapter() });
+
+describe("Puebas en MultipleCostumsHooks", () => {
+  test("Should show the correct result", () => {
+    useFetch.mockReturnValue({
+      data: null,
+      loading: false,
+      error: null,
+    });
+    const wrapper = shallow(<MultipleCustomsHooks />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  test("Should show the information of the quote It", () => {
+    useFetch.mockReturnValue({
+      data: [{ author: "Yeison", quotes: "Hola mundo" }],
+      loading: false,
+      error: null,
+    });
+    const wrapper = shallow(<MultipleCustomsHooks />);
+    expect(wrapper.find(".alert").exists()).toBe(false);
+    console.log(wrapper.html());
+  });
+});
